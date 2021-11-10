8b0f3c060df9b8b1e448e606f0534a87028c98b1
ghouston16 <ghouston16@hotmail.com>
2021-11-10 13:11:12 +0000

    binding for donations and progress bars

--- app/src/main/java/org/wit/donationx/Donate.kt
+++ app/src/main/java/org/wit/donationx/Donate.kt
@@ -2,16 +2,36 @@ package org.wit.donationx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import org.wit.donationx.databinding.ActivityDonateBinding
 
 class Donate : AppCompatActivity() {
+    // Enable binding on the layout views
     private lateinit var donateLayout: ActivityDonateBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         donateLayout = ActivityDonateBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(donateLayout.root)
+        // Progress bar for donation targets
         donateLayout.progressBar.max = 10000
+        // Max and Min values for number picker for donation amount
         donateLayout.amountPicker.minValue = 1
         donateLayout.amountPicker.maxValue = 1000
+        donateLayout.amountPicker.setOnValueChangedListener { _, _, newVal ->
+            //Display the newly selected number to paymentAmount
+            donateLayout.paymentAmount.setText("$newVal")
+        }
+        var totalDonated = 0
+        donateLayout.donateButton.setOnClickListener{
+            val amount = if (donateLayout.paymentAmount.text.isNotEmpty())
+                    donateLayout.paymentAmount.text.toString().toInt() else donateLayout.amountPicker.value
+            if (totalDonated >= donateLayout.progressBar.max)
+                Toast.makeText(applicationContext ,"Donate Target Reached!",Toast.LENGTH_LONG).show()
+                else{
+                    totalDonated += amount
+                    donateLayout.totalSoFar.text = "$totalDonated"
+                    donateLayout.progressBar.progress = totalDonated
+            }
+        }
     }
 }
